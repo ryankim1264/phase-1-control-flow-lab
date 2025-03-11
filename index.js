2f30c000e783203f7977a143cf2c4216e7dcbcc9
Ryan mugei <Lquid1264@gmail.com>
2025-03-11 08:15:21 +0300

    Done

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){}
   // Write your code here!
+  function scuberGreetingForFeet(distanceInFeet){
+    if (distanceInFeet <=400 ) {
+    return "This one is on me!";
+    }  else if (distanceInFeet> 400 && distanceInFeet <= 2000) {
+      return "That will be twenty bucks.";
+    } else if(distanceInFeet > 2000 && distanceInFeet <= 2500){
+      return "I will gladly take your thirty bucks.";
+    }else {
+      return "No can do.";
+    }
+      
+
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-}
+   if( city === "NYC") 
+    {return "Ok, sounds good.";}
+    
+    else if (city !== "NYC"){return "No go.";}
+  
+     
+    
+    }
 
-function switchOnCharmFromTip(){
+
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
+  switch (tip){
+    case "generous":
+    return"Thank you so much.";
+    case "not as generous":
+    return "Thank you.";
+    case "anything else":
+      return "Bye."
+
+  }
+    
+  }
+  
